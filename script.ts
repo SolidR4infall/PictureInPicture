c04dfd0431b3cde712e4e5c5029288db5c61073b
Gabe369 <novellogab@gmail.com>
2023-07-30 21:05:33 -0400

    complete

--- script.ts
+++ script.ts
@@ -1,27 +1,37 @@
-"use strict";
-const videoElement = document.getElementById("video");
-const button = document.getElementById("button");
+
+const videoElement = document.getElementById("video") as HTMLVideoElement;
+const button = document.getElementById("button") as HTMLButtonElement;
 const reset = document.getElementById("reset");
+
+
 // Prompt to select media stream, pass to video element, then play
-async function selectMediaStream() {
-    try {
+async function selectMediaStream() : Promise<any>
+{
+    try 
+    {
         const mediaStream = await navigator.mediaDevices.getDisplayMedia(); // Waits for user to select screen
         videoElement.srcObject = mediaStream; // Sets mediaStream to source object
-        videoElement.onloadedmetadata = () => { videoElement.play(); }; // Plays video when loaded
+        videoElement.onloadedmetadata = () => {videoElement.play();}; // Plays video when loaded
     }
-    catch {
+    catch 
+    {
         // Catch error here
         console.log("whoops, error");
     }
 }
+
 button.addEventListener("click", async () => {
     // Disable button
     button.disabled = true; // Button disabled while requesting
     // Start Picture in Picture
-    await videoElement.requestPictureInPicture(); // Waits for video element request
+    await videoElement.requestPictureInPicture();  // Waits for video element request
     // Reset Button
     button.disabled = false;
-});
-reset?.addEventListener("click", async () => { window.location.reload(); });
+} )
+
+reset?.addEventListener("click", async () => {window.location.reload();})
+
+
+
 // On load
 selectMediaStream();
